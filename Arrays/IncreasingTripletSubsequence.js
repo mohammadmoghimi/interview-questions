79311d3dd6f30f4e2385649274f96fd49c87c8cd
mohammadmoghimi <mmoghimi2003@gmail.com>
2024-04-30 09:52:28 +0430

    day sixty-one

---
+++ Arrays/IncreasingTripletSubsequence.js
@@ -0,0 +1,40 @@
+// Given an integer array nums, return true if there exists 
+// a triple of indices (i, j, k) such that 
+// i < j < k and nums[i] < nums[j] < nums[k]. 
+// If no such indices exists, return false.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5]
+// Output: true
+// Explanation: Any triplet where i < j < k is valid.
+// Example 2:
+
+// Input: nums = [5,4,3,2,1]
+// Output: false
+// Explanation: No triplet exists.
+// Example 3:
+
+// Input: nums = [2,1,5,0,4,6]
+// Output: true
+// Explanation: The triplet (3, 4, 5) is valid 
+// because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+var increasingTriplet = function(nums) {
+    let smallest = Infinity;
+    let secondSmallest = Infinity;
+    
+    for (let num of nums) {
+        if (num <= smallest) {
+            smallest = num;
+        } else if (num <= secondSmallest) {
+            secondSmallest = num;
+        } else {
+            return true;
+        }
+    }
+    
+    return false;
+};

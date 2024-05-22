a32bacb3f4d0739ce47e03e1969085ad7a63d1a2
mohammadmoghimi <mmoghimi2003@gmail.com>
2024-05-22 12:14:55 +0430

    day eighty-three

---
+++ Trees and Graphs/BinaryTreeMaximumPathSum.js
@@ -0,0 +1,41 @@
+// A path in a binary tree is a sequence of nodes 
+// where each pair of adjacent nodes in the sequence 
+// has an edge connecting them. A node can only 
+// appear in the sequence at most once. Note that 
+// the path does not need to pass through the root.
+
+// The path sum of a path is the sum of the node's values in the path.
+
+// Given the root of a binary tree, return the 
+// maximum path sum of any non-empty path.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3]
+// Output: 6
+// Explanation: The optimal path is 2 -> 1 -> 3 
+// with a path sum of 2 + 1 + 3 = 6.
+// Example 2:
+
+
+// Input: root = [-10,9,20,null,null,15,7]
+// Output: 42
+// Explanation: The optimal path is 15 -> 20 -> 7 
+// with a path sum of 15 + 20 + 7 = 42.
+
+var maxPathSum = function(root) {
+    let maxSum = -Infinity;
+const dfs = (node) => {
+    if (!node) return 0;
+    const leftMax = Math.max(dfs(node.left), 0);
+    const rightMax = Math.max(dfs(node.right), 0);
+    const currentMax = node.val + leftMax + rightMax;
+    maxSum = Math.max(maxSum, currentMax);
+    return node.val + Math.max(leftMax, rightMax);
+};
+dfs(root);
+return maxSum;
+};

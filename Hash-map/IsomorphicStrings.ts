6e0751a50c307593f0d08e2ff884d111c5d7c2f0
mohammadmoghimi <mmoghimi2003@gmail.com>
2024-06-16 09:40:33 +0430

    day 100

---
+++ Hash-map/IsomorphicStrings.ts
@@ -0,0 +1,52 @@
+// Given two strings s and t, determine if they are isomorphic.
+
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+// Example 1:
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
+
+// Input: s = "paper", t = "title"
+// Output: true
+
+function isIsomorphic(s: string, t: string): boolean {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const mapSToT: { [key: string]: string } = {};
+    const mapTToS: { [key: string]: string } = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        if (charS in mapSToT) {
+            if (mapSToT[charS] !== charT) {
+                return false;
+            }
+        } else {
+            mapSToT[charS] = charT;
+        }
+
+        if (charT in mapTToS) {
+            if (mapTToS[charT] !== charS) {
+                return false;
+            }
+        } else {
+            mapTToS[charT] = charS;
+        }
+    }
+
+    return true;
+};
